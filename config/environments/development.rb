429c423928f67ab005dd463bcdc0b92e00c8ab5d
kiyo <kiyo324783@icloud.com>
2021-03-08 02:48:27 +0000

    Add account activation

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,7 +33,11 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
-
+  
+  host = '7ce0886fd9364460813bdb9be7ba77ad.vfs.cloud9.us-east-2.amazonaws.com'
+  
+  config.action_mailer.default_url_options = { host: host, protocol: 'https' }
+  
   config.action_mailer.perform_caching = false
 
   # Print deprecation notices to the Rails logger.
